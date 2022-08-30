1debab3582a92dd8098230e252b19f582fe43548
Theresa2013 <tab61295@gmail.com>
2022-08-29 19:56:38 -0500

    changed query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,16 +1,6 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-<<<<<<< HEAD
-  type User {
-    _id: ID
-    username: String
-    email: String
-  }
-`;
-
-module.exports = typeDefs;
-=======
   type Query {
     helloWorld: String
   }
@@ -18,4 +8,3 @@ module.exports = typeDefs;
 
 // export typeDefs
 module.exports = typeDefs;
->>>>>>> mutations
