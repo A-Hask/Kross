567103e660573318d0d207eeb3187d11277e5d9b
Qu33nArlene <ahaskins93@yahoo.com>
2022-08-30 19:16:22 -0400

    fixed mistake in typeDefs file. All good now

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -33,7 +33,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    addPost(postText: String!): Thought
+    addPost(postText: String!): Post
     addComment(commentId: ID!, commentBody: String!): Post
     addKrossie(krossieId: ID!): User
   }
