ce97ed346cae639cf7ee2e203a45c1f95e44cf12
Nadya <hoperu1986@gmail.com>
2022-08-31 14:50:57 -0400

    connected server and client, added footer, header to front end

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main>
+      <div className='flex-row justify-space-between'>
+        <div className='col-12 mb-3'>Nice gaming picture and sign in/sign up forms</div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
