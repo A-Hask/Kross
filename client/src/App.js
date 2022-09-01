6c55639c455bf9db3f2dafda99574267f84ce674
BeauJax <beaujackson88@yahoo.com>
2022-08-31 20:44:24 -0400

    profile/sign up page

--- client/src/App.js
+++ client/src/App.js
@@ -12,11 +12,9 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
-import NoMatch from './pages/NoMatch';
-import SingleThought from './pages/SingleThought';
+import NoMatch from './pages/NoMatch';;
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
-
 
 const httpLink = createHttpLink({
     uri: '/graphql',
