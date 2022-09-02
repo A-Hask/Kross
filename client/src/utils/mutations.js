f4958f79dafe39cf5943228dd67decbeaf98fe70
Qu33nArlene <ahaskins93@yahoo.com>
2022-09-01 20:40:55 -0400

    added queries.js and mutations.js to utils

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -22,3 +22,60 @@ export const ADD_USER = gql`
         }
     }
 `;
+
+export const ADD_POST = gql`
+  mutation addPost($postText: String!) {
+    addPost(postText: $postText) {
+      _id
+      postText
+      createdAt
+      username
+      commentCount
+      comments {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+  mutation addComment($postId: ID!, $commentBody: String!) {
+    addComment(postId: $postId, commentBody: $commentBody) {
+      _id
+      commentCount
+      comments {
+        _id
+        commentBody
+        createdAt
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_KROSSIE = gql`
+  mutation addKrossie($id: ID!) {
+    addKrossie(krossieId: $id) {
+      _id
+      username
+      krossieCount
+      krossies {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const REMOVE_KROSSIE = gql`
+  mutation removeKrossie($id: ID!) {
+    removeKrossie(id: $id) {
+      _id
+      username
+      krossies {
+        _id
+        username
+      }
+    }
+  }
+`;
