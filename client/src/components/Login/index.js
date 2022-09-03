3ad213f5e3123906bc4928a7849142a144056ab2
Qu33nArlene <ahaskins93@yahoo.com>
2022-09-02 21:10:07 -0400

    added handleFormSubmit functions to login and signup

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
+import { LOGIN_USER } from "../../utils/mutations";
+import Auth from "../../utils/auth";
+import { useMutation } from "@apollo/client";
 
 const Login = () => {
+  const [login, { error }] = useMutation(LOGIN_USER);
+  const [formState, updateFormState] = useState({ email: "", password: "" });
+
+  const handleFormSubmit = async (event) => {
+    event.PreventDefault();
+    try {
+      const { response } = await login({ variables: { ...formState } });
+      Auth.login(response.login.token);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    updateFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
   return (
     <main className="w-100 mt-auto bg-secondary p-4">
       <form>
@@ -9,18 +33,24 @@ const Login = () => {
           <div>
             <label>Email:</label>
             <input
-              type='email'
-              placeholder='Enter Email'
+              type="email"
+              placeholder="Enter Email"
+              name="email"
+              value={formState.email}
+              onChange={handleChange}
             />
           </div>
           <div>
             <label>Password:</label>
             <input
-              type='email'
-              placeholder='Enter Password'
+              type="password"
+              placeholder="Enter Password"
+              name="password"
+              value={formState.password}
+              onChange={handleChange}
             />
             <br></br>
-            <button>Submit</button>
+            <button onClick={handleFormSubmit}>Submit</button>
           </div>
         </div>
       </form>
--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -1,35 +1,64 @@
-import React from 'react';
+import React, { useState } from "react";
+import { ADD_USER } from "../../utils/mutations";
+import Auth from "../../utils/auth";
+import { useMutation } from "@apollo/client";
 
 const Signup = () => {
+  const [signup, { error }] = useMutation(ADD_USER);
+  const [formState, updateFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleFormSubmit = async (event) => {
+    event.PreventDefault();
+    try {
+      const { response } = await signup({ variables: { ...formState } });
+      Auth.signup(response.signup.token);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <main className="w-100 mt-auto bg-secondary p-4">
       <form>
         <div>
           <h3>Sign Up</h3>
         </div>
+        <div className="container"></div>
+        <label>Username:</label>
+        <input
+          type="username"
+          placeholder="Enter Username"
+          name="username"
+          value={formState.email}
+          onChange={handleChange}
+        />
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            placeholder="Enter Email"
+            name="email"
+            value={formState.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="password"
+            value={formState.email}
+            onChange={handleChange}
+          />
+          <br></br>
+          <button onClick={handleFormSubmit}>Submit</button>
+        </div>
       </form>
-      <div className="container"></div>
-      <label>Username:</label>
-      <input
-        type="username"
-        placeholder='Enter Username'
-      />
-      <div>
-        <label>Email:</label>
-        <input
-          type='email'
-          placeholder='Enter Email'
-        />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input
-          type='password'
-          placeholder='Enter Password'
-        />
-        <br></br>
-        <button>Submit</button>
-      </div>
     </main>
   );
 };
